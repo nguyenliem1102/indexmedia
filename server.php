7adf33af5643d39ae925f410e5a7299108b88b69
nguyenliem1102 <nguyenliem1102@gmail.com>
2014-04-13 11:35:00 +0700

    update server code
    update small

--- server.php
+++ server.php
@@ -1,8 +1,16 @@
 <?php
-		$regstration = $_POST['registration']; 
-		$name= $_POST['name']; 
-		$email= $_POST['email'];
-		$links = $_POST['links']; 
-		echo "Posted";
+		
+		if(isset($_POST['registration']) && !empty($_POST['registration'])) {
+		   $regstration = $_POST['registration']; 
+			$name= $_POST['name']; 
+			$email= $_POST['email'];
+			$links = $_POST['links']; 
+			switch($name) {
+				case 'Andy' : 		echo "Andy Posted";break;
+				case 'blah' : echo "blah Posted";break;
+				
+			}
+		}
+
 		
 ?>
